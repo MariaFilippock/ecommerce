eef2688cbd5048b0d77bceb50b6c0303dba1ffe2
MariaFilippock <Kasyan96@yandex.ru>
2026-01-08 20:21:47 +0200

    добавлен бек для favorites

--- frontend/src/pages/FavoritesPage/FavoritesPage.tsx
+++ frontend/src/pages/FavoritesPage/FavoritesPage.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useSelector} from 'react-redux';
 import {IAppState, ItemType} from '../../models';
 import Item from '../../components/Item/Item';
 import {ROUTES} from '../../const';
 import {useNavigate} from 'react-router-dom';
+import {useAppDispatch} from '../../store/hooks';
+import {setFavoritesThunk} from '../../store/favorites-thunk';
 
 
 const FavoritesPage = () => {
     const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
     const selectFavoritesItems = (state: IAppState) =>
         state.itemsData.favorites
@@ -16,6 +19,10 @@ const FavoritesPage = () => {
             ).filter((item): item is ItemType => item !== undefined);
 
     const favoritesList = useSelector(selectFavoritesItems);
+
+    useEffect(() => {
+        dispatch(setFavoritesThunk());
+    }, [dispatch]);
 
     const handleItemDetailCardClick = (id: number) => {
         navigate(`${ROUTES.ITEMS}/${id}`);
