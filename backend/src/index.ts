6e0d5db4f9ff70412da26eeae9aef7d87ffe249d
MariaFilippock <Kasyan96@yandex.ru>
2026-01-14 11:25:51 +0200

    добавлены изменения

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,17 +3,22 @@ import path from 'path';
 import cartRouter from './routes/cart';
 import favoritesRouter from './routes/favorites';
 import productsRouter from './routes/products';
+import cors from "cors";
 
 const app = express(); // создаём сервер
 const PORT = process.env.PORT || 5001;
 
+
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json()); //подключает middleware для разбора JSON в теле запроса, учим сервер понимать JSON
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok', uptime: process.uptime() });
 });
 
-// app.use('/img', express.static(path.join(__dirname, '../public/img')));
+app.use('/img', express.static(path.join(__dirname, '../public/img')));
 
 app.use('/api', productsRouter);
 app.use('/api', cartRouter);
