60a15e4825c7c576ef132123a95cced1a1fc4a0a
MariaFilippock <Kasyan96@yandex.ru>
2026-01-15 11:01:16 +0200

    добавлены изменения

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5001;
 
 
 app.use(cors({
-    origin: "https://ecommerce-front-3tbu.onrender.com"
+    origin: "*"
 }));
 
 app.use(express.json()); //подключает middleware для разбора JSON в теле запроса, учим сервер понимать JSON
